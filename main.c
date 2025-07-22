3cb9ec7f1e134f5efe0d792d9dd6fec056b6ce44
Mordegb <cadumalmeida13@gmail.com>
2025-07-22 11:48:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pessoa.c"
+#include "estado.c"
+//#include "eleicao.c"
+enum {estado = 1, pessoa = 2, eleicaao = 3 ,};
+
+
+
+
+int main() {
+
+    printf("==================================\n");
+    printf("  trabalho eduardo almeida 2025  \n");
+    printf("==================================\n");
+
+    int opcao;
+    do {
+        printf("oque deseja realizar em seu banco de dados?.\n");
+        printf("1 - configurar estado. 2 - configurar pessoa.\n");
+        printf("3 - configurar eleicao.\n");
+        scanf("%d", &opcao);
+        limpar_getchar();
+
+        switch (opcao) {
+            case estado:
+                func_main_uf();
+                break;
+
+            case pessoa:
+               func_main_pessoa();
+                break;
+            case eleicaao:
+              //  func_main_eleicao();
+                break;
+            case 4:
+                editar_estado();
+                break;
+            case 5:
+                remover_estado();
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (opcao != 0);
+
+
+    return 0;
+}#include <stdio.h>
+#include <stdlib.h>
 #include "pessoa.h"
 #include "estado.h"
 
